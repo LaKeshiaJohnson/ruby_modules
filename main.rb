b6fa96e0df96b13df0977f0349e5844de584987c
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-10-10 19:21:47 -0500

    completed

--- main.rb
+++ main.rb
@@ -5,4 +5,4 @@ require_relative 'table'
 the_table = Table.new()
 puts the_table.description
 puts the_table.seating
-puts the_table.total_size(50,50,20)
+puts the_table.total_size("50 inches","50 inches","20 inches")
